63120ef6efa16f014ef8ae427b04b512d2bd920e
atabong <tajoacha@gmail.com>
2023-07-14 10:57:10 +0300

    devs

--- src/App.js
+++ src/App.js
@@ -24,38 +24,38 @@ function App() {
   const [toggle, setToggle] = useState(false)
 
   const loadBlockchainData = async () => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const provider = new ethers.providers.Web3Provider(window.ethereum)   //connects to the blockchain
     setProvider(provider)
 
-    const network = await provider.getNetwork()
-    const tokenMaster = new ethers.Contract(config[network.chainId].TokenMaster.address, TokenMaster, provider)
-    setTokenMaster(tokenMaster)
+    const network = await provider.getNetwork() //gets the networks as they change
+    const tokenMaster = new ethers.Contract(config[network.chainId].TokenMaster.address, TokenMaster, provider) 
+    setTokenMaster(tokenMaster)//smart contract is here in webform her
 
-    const totalOccasions = await tokenMaster.totalOccasions()
+    const totalOccasions = await tokenMaster.totalOccasions() //gets the number of occaccsion on chain
     const occasions = []
 
-    for (var i = 1; i <= totalOccasions; i++) {
+    for (var i = 1; i <= totalOccasions; i++) { //gets the occasions into the the occasions array
       const occasion = await tokenMaster.getOccasion(i)
       occasions.push(occasion)
     }
 
     setOccasions(occasions)
 
-    window.ethereum.on('accountsChanged', async () => {
+    window.ethereum.on('accountsChanged', async () => {  //metamask is connected when when clicked
       const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-      const account = ethers.utils.getAddress(accounts[0])
+      const account = ethers.utils.getAddress(accounts[0])//gets the the address of the account at the oth index
       setAccount(account)
     })
   }
 
   useEffect(() => {
-    loadBlockchainData()
+    loadBlockchainData() //loads the blockchain data
   }, [])
 
   return (
     <div>
       <header>
-        <Navigation account={account} setAccount={setAccount} />
+        <Navigation account={account} setAccount={setAccount} /> 
 
         <h2 className="header__title"><strong>Event</strong> Tickets</h2>
       </header>
